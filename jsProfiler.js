8bc1b07b34ee3fa53baf469dd7d7b147bec03945
Jenna Zeigen <jenna.zeigen@gmail.com>
2012-11-13 20:18:07 -0500

    profiles times, with bar graph

--- jsProfiler.js
+++ jsProfiler.js
@@ -3,69 +3,140 @@
 // * Breakdown *
 // 1. Find all the functions
 // 2. Wrap them in another function that
-// 	a) times the function ('clocker')
-// 	b) figures out which functions it calls
-//	c) logs all the info for future viz ('logger')
+//    a) times the function ('clocker')
+//    b) figures out which functions it calls
+//    c) logs all the info for future viz ('logger')
 
 //object representation of the information the visualizer will show
 function FunctionObject(time) {
-	this.runs = []
-	this.parents = []
+    this.runs = []
+    this.parents = []
 };
 
 //constant time, yo.
 var functionsTracked = {};
+var svg;
 
 var traverser = function(object) {
-	$.each(object, function(key, value) {
-		if (typeof(value) === 'function') {
-			object[key] = clocker(value, key)
-			functionsTracked[key] = new FunctionObject()
-		}
-		else if (typeof(value) === 'object'){
-			traverser(object)
-		}
-	})
+    $.each(object, function(key, value) {
+        if (typeof(value) === 'function') {
+            object[key] = clocker(value, key)
+            functionsTracked[key] = new FunctionObject()
+        }
+        else if (typeof(value) === 'object'){
+            traverser(value)
+        }
+    })
 }
 
 //takes in a function and wraps it in a timing scheme, and stores that information in the function's FunctionObject representation
 //TODO: make the wrapper take as least time as possible because it'll slow down any nested functions... umm.
 var clocker = function(toTime, name) {
-	var clocked = function() {
-		//time it
-		var start = new Date().getMilliseconds();
-		var retVal = toTime.apply(this, arguments);
-		var end = new Date().getMilliseconds();
-		functionsTracked[name].runs.push(end-start)
-		functionsTracked[name].parents[functionsTracked[name].runs.length]=[]
-		pushParents(name, arguments)
-		return retVal;
-	}
-	return clocked
+    var clocked = function() {
+        //time it
+        var start = new Date().getMilliseconds();
+        var retVal = toTime.apply(this, arguments);
+        var end = new Date().getMilliseconds();
+        functionsTracked[name].runs.push(end-start)
+        functionsTracked[name].parents[functionsTracked[name].runs.length]=[]
+        pushParents(name, arguments)
+        return retVal;
+    }
+    return clocked
 }
 
 var pushParents = function(name, args) {
-	console.log(args)
-	if (args.length>0){
-		functionsTracked[name].parents[functionsTracked[name].runs.length].push(args.callee.caller.name)
-		pushParents(name, args.callee.caller.arguments)
-	}
+    if (args.length>0){
+        functionsTracked[name].parents[functionsTracked[name].runs.length].push(args.callee.caller.name)
+        pushParents(name, args.callee.caller.arguments)
+    }
 }
 
-// clocker(function(num){
-// 	var arr = [];
-// 		for (var i=0; i<num; i++){
-// 		$.each([1,2,3,4], function(i,v){
-// 			arr.push(v + i)
-// 		})
-// 	};
-// })(50000)
+
+//*** VISUALIZATIONS!! ***
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+	var x = d3.scale.ordinal()
+	    .rangeRoundBands([0, width], .1);
+
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+
+var drawBars = function() {
+	var points = [];
+	$.each(functionsTracked, function(k,v) {points.push( {name: k, avgT: d3.mean(v.runs)} )})
+
+	svg = d3.select("body").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  	.append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	  x.domain(points.map(function(d) { return d.name; }));
+	  y.domain([0, d3.max(points, function(d) { return d.avgT; })]);
+
+	  svg.append("g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(xAxis);
+
+	  svg.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis)
+	    .append("text")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 6)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end")
+	      .text("Average Time (ms)");
+
+	  	svg.selectAll(".bar")
+	      .data(points)
+	    .enter().append("rect")
+	      .attr("class", "bar")
+	      .attr("x", function(d) { return x(d.name); })
+	      .attr("width", x.rangeBand())
+	      .attr("y", function(d) { return y(d.avgT); })
+	      .attr("height", function(d) { return height - y(d.avgT); });
+}
+
+var redraw = function() {
+	var points = [];
+	$.each(functionsTracked, function(k,v) {points.push( {name: k, avgT: d3.mean(v.runs)} )})
+	console.log(points)
+
+	console.log('redraw')
+ 	svg.selectAll('rect')
+ 		.data(points)
+ 		.transition()
+ 		.duration(500)
+		.attr("x", function(d) { return x(d.name); })
+	      .attr("width", x.rangeBand())
+	      .attr("y", function(d) { return y(d.avgT); })
+	      .attr("height", function(d) { return height - y(d.avgT); });
+
+	svg.selectAll('text')
+		.data(points)
+		.transition()
+		.duration()
+		.text(function(d) {return d.name})
+ }
+
+setTimeout(function(){
+	drawBars();
+}, 1000);
 
 
-// 2 + 4 * 10
-
-//     +
-//   /   \
-//  2     *
-//       / \
-//       4  10 
+setInterval(function(){
+	redraw();
+}, 1000);
